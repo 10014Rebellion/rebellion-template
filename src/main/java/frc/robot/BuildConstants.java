9b7788d4c46a7dfdc9d3718ccee5fee64caee7cf
Rylan Moseley <rylan.moseley@revrobotics.com>
2025-06-24 11:47:06 -0500

    Update pre-commit script

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "rebellion-template";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "abff71d0c6f58f23c44bc4c5c1e01fc3f48f53c6";
+  public static final String GIT_DATE = "2025-06-24 12:07:08 EDT";
+  public static final String GIT_BRANCH = "add-ci-and-formatting";
+  public static final String BUILD_DATE = "2025-06-24 12:47:08 EDT";
+  public static final long BUILD_UNIX_TIME = 1750783628995L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
