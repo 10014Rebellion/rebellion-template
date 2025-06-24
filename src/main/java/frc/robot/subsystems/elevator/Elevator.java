ce48d7102a4f3ff2c980a2aa145e5f28f565cb56
Rylan Moseley <rylan.moseley@revrobotics.com>
2025-06-24 13:04:45 -0500

    update spotless and switch style to palantir

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -3,8 +3,8 @@ package frc.robot.subsystems.elevator;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
-  public Elevator() {}
+    public Elevator() {}
 
-  @Override
-  public void periodic() {}
+    @Override
+    public void periodic() {}
 }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -5,59 +5,59 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 public class ElevatorConstants {
-  public static final SparkMaxConfig kElevatorConfig = new SparkMaxConfig();
+    public static final SparkMaxConfig kElevatorConfig = new SparkMaxConfig();
 
-  public class ElevatorMotor {
-    public static int kMotorID = 41;
-    public static MotorType kMotorType = MotorType.kBrushless;
-    public static IdleMode kIdleMode = IdleMode.kBrake;
-    public static int kCurrentLimitAmp = 60;
-    public static boolean kMotorInverted = false;
-  }
+    public class ElevatorMotor {
+        public static int kMotorID = 41;
+        public static MotorType kMotorType = MotorType.kBrushless;
+        public static IdleMode kIdleMode = IdleMode.kBrake;
+        public static int kCurrentLimitAmp = 60;
+        public static boolean kMotorInverted = false;
+    }
 
-  public class ElevatorEncoder {
-    public static boolean kEncoderInverted = false;
-    public static double kPositionConversionFactor = 1;
-    public static double kVelocityConversionFactor = 1;
-  }
+    public class ElevatorEncoder {
+        public static boolean kEncoderInverted = false;
+        public static double kPositionConversionFactor = 1;
+        public static double kVelocityConversionFactor = 1;
+    }
 
-  public class ElevatorController {
-    public static double kP = 0;
-    public static double kI = 0;
-    public static double kD = 0;
+    public class ElevatorController {
+        public static double kP = 0;
+        public static double kI = 0;
+        public static double kD = 0;
 
-    public static double kG = 0;
-    public static double kS = 0;
-    public static double kV = 0;
-    public static double kA = 0;
+        public static double kG = 0;
+        public static double kS = 0;
+        public static double kV = 0;
+        public static double kA = 0;
 
-    public static double kMaxVelocity = 0;
-    public static double kMaxAcceleration = 0;
-  }
+        public static double kMaxVelocity = 0;
+        public static double kMaxAcceleration = 0;
+    }
 
-  public enum ElevatorStates {
-    NONE,
-    L1,
-    L2,
-    L3,
-    L4,
-    BARGE,
-    PROCESSOR
-  }
+    public enum ElevatorStates {
+        NONE,
+        L1,
+        L2,
+        L3,
+        L4,
+        BARGE,
+        PROCESSOR
+    }
 
-  static {
-    kElevatorConfig
-        .idleMode(ElevatorMotor.kIdleMode)
-        .smartCurrentLimit(ElevatorMotor.kCurrentLimitAmp)
-        .inverted(ElevatorMotor.kMotorInverted)
-        .voltageCompensation(12.0);
+    static {
+        kElevatorConfig
+                .idleMode(ElevatorMotor.kIdleMode)
+                .smartCurrentLimit(ElevatorMotor.kCurrentLimitAmp)
+                .inverted(ElevatorMotor.kMotorInverted)
+                .voltageCompensation(12.0);
 
-    kElevatorConfig
-        .encoder
-        .inverted(ElevatorEncoder.kEncoderInverted)
-        .positionConversionFactor(1)
-        .velocityConversionFactor(1)
-        .uvwMeasurementPeriod(10)
-        .uvwAverageDepth(2);
-  }
+        kElevatorConfig
+                .encoder
+                .inverted(ElevatorEncoder.kEncoderInverted)
+                .positionConversionFactor(1)
+                .velocityConversionFactor(1)
+                .uvwMeasurementPeriod(10)
+                .uvwAverageDepth(2);
+    }
 }
