51b246b7aab8cc825766a982e87752741be2b19a
Rylan Moseley <rylan.moseley@revrobotics.com>
2025-06-24 11:56:54 -0500

    Update pre-commit hook

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "rebellion-template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "abff71d0c6f58f23c44bc4c5c1e01fc3f48f53c6";
-  public static final String GIT_DATE = "2025-06-24 12:07:08 EDT";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "321a72f484586d30bcdf166c948921143e8f9c2a";
+  public static final String GIT_DATE = "2025-06-24 12:52:33 EDT";
   public static final String GIT_BRANCH = "add-ci-and-formatting";
-  public static final String BUILD_DATE = "2025-06-24 12:47:08 EDT";
-  public static final long BUILD_UNIX_TIME = 1750783628995L;
+  public static final String BUILD_DATE = "2025-06-24 12:55:10 EDT";
+  public static final long BUILD_UNIX_TIME = 1750784110944L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
