48863755475dc51d06983fa4d5f66e88f8015119
Rylan Moseley <rylan.moseley@revrobotics.com>
2025-07-18 16:33:01 -0500

    add submodule utility scripts

---
+++ scripts/check-submodule.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <submodule_path>"
+    exit 1
+fi
+
+if [ ! -d ./gradle ]; then
+    echo "Error: Gradle directory does not exist. Please run this script from the root of the project."
+    exit 1
+fi
+
+if [ -d "$1" ]; then
+    echo "Submodule path provided: $1"
+else
+    echo "Error: Submodule path does not exist or is not a directory."
+    exit 1
+fi
+
+PROJECT_ROOT=$(pwd)
+# ./check-submodule.sh <submodule_path>
+SUBMODULE=$1
+
+# check for local updates to submodule
+cd $SUBMODULE
+if [ -z "$(git status --porcelain)" ]; then
+    if [ -z "$(git log @{u}..)" ]; then
+        echo "Submodule is up to date."
+    else
+        echo "Submodule has un-pushed changes."
+        exit 1
+    fi
+else 
+    echo "Submodule has uncommitted changes."
+    exit 1
+fi
+
+# check if submodule is on a non-main branch
+if [ -n "$(git branch --show-current | grep -i -v -E 'main|master')" ]; then
+    echo "Submodule is on a non-main branch."
+    exit 1
+fi
+
+cd "$PROJECT_ROOT"
+
+# check for remote updates to submodule
+git add $SUBMODULE
+BEFORE_UPDATE=$(git submodule status)
+git submodule update --init --recursive --remote --merge
+
+if [ "$BEFORE_UPDATE" != "$(git submodule status)" ]; then
+    echo "Submodule is not the latest version."
+    exit 1
+else
+    echo "No changes in submodules."
+    exit 0
+fi
