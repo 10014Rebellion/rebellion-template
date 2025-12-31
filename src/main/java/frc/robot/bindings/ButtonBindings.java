3b387fe91d1a3669743fca749a6e689df85b565f
RebellionGit10014 <rebellion.git@gmail.com>
2025-12-30 19:04:08 -0600

    Added autoalign and reset gyro bindings

--- src/main/java/frc/robot/bindings/ButtonBindings.java
+++ src/main/java/frc/robot/bindings/ButtonBindings.java
@@ -2,8 +2,12 @@
 
 package frc.robot.bindings;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.controllers.FlydigiApex4;
 import frc.robot.systems.drive.Drive;
+import frc.robot.systems.drive.controllers.HolonomicController.ConstraintType;
 
 public class ButtonBindings {
     private final Drive mDriveSS;
@@ -20,5 +24,13 @@ public class ButtonBindings {
                 () -> -mDriverController.getLeftX(),
                 () -> -mDriverController.getRightX(),
                 () -> mDriverController.getPOVAngle());
+
+        mDriverController.y().onTrue(Commands.runOnce(() -> mDriveSS.resetGyro()));
+
+        mDriverController.a()
+            .onTrue(
+                Commands.runOnce(() -> mDriveSS.setPose(new Pose2d()), mDriveSS)
+                    .andThen(mDriveSS.setToGenericAutoAlign(() -> new Pose2d(0.5, 0, Rotation2d.kCCW_Pi_2), ConstraintType.LINEAR)))
+            .onFalse(mDriveSS.setToTeleop());
     }
 }
