d8233e82bcd3ca047f0a77181265514716708ded
RebellionGit10014 <rebellion.git@gmail.com>
2025-12-30 21:24:57 -0600

    Update BindingsConstants.java

--- src/main/java/frc/robot/bindings/BindingsConstants.java
+++ src/main/java/frc/robot/bindings/BindingsConstants.java
@@ -8,9 +8,19 @@ public class BindingsConstants {
     public static final int kDriverControllerPort = 0;
 
     public static final DriverProfiles kDefaultProfile =
-            new DriverProfiles(1, 3, 0.075, 1.0, 3.0, 0.1, 0.2, true, "Default");
+            new DriverProfiles(
+                1, // Linear Scalar
+                3,  // Linear Exponent
+                0.075, // Left Joystick Deadband
+                1.0, // Rotational Scalar
+                3.0, // Rotational Exponent
+                0.1, // Right Joystick Deadband
+                0.2, // Sniper Scalar
+                true, // Swap Sides on Reef
+                "Default" // Name
+            );
 
     public static DriverProfiles[] kProfiles = {
-        kDefaultProfile, new DriverProfiles(1, 3, 0.075, 0.8, 3.0, 0.1, 0.2, false, "Taha The GOAT")
+        kDefaultProfile, new DriverProfiles(1, 3, 0.075, 1, 3.0, 0.1, 0.2, false, "Taha The GOAT")
     };
 }
